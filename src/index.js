2e26e2ffe90c8f1274e6b1e836aa04cff3320c68
Wolwer1ne <wolwerine@yandex.ru>
2022-02-07 15:23:53 +0300

    Setup for Lab 1

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import Rectangle from "./rectangle";
+
+const canvas = document.getElementById("cnvs");
+
+const gameState = {};
+
+function queueUpdates(numTicks) {
+    for (let i = 0; i < numTicks; i++) {
+        gameState.lastTick = gameState.lastTick + gameState.tickLength
+        update(gameState.lastTick)
+    }
+}
+
+function draw(tFrame) {
+    const context = canvas.getContext('2d');
+
+    // clear canvas
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    gameState.figures.each((figure)=>{
+        context.rect(figure)
+    })
+    // draw
+}
+
+function update(tick) {
+
+}
+
+function run(tFrame) {
+    gameState.stopCycle = window.requestAnimationFrame(run)
+
+    const nextTick = gameState.lastTick + gameState.tickLength
+    let numTicks = 0
+
+    if (tFrame > nextTick) {
+        const timeSinceTick = tFrame - gameState.lastTick
+        numTicks = Math.floor(timeSinceTick / gameState.tickLength)
+    }
+    queueUpdates(numTicks)
+    draw(tFrame)
+    gameState.lastRender = tFrame
+}
+
+function stopGame(handle) {
+    window.cancelAnimationFrame(handle);
+}
+
+function setup() {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    gameState.lastTick = performance.now()
+    gameState.lastRender = gameState.lastTick
+    gameState.tickLength = 15 //ms
+
+    gameState.figures = []
+    gameState.figures.push(new Rectangle(10,10, 30, 30))
+
+}
+
+setup();
+run();
